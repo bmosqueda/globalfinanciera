6b9008772075354ae8ea448cdfd4bfd31df37cfb
CarlosRDGZ <crodriguez20@ucol.mx>
2018-09-01 18:04:49 -0500

    inicio faq

---
+++ faq.php
@@ -0,0 +1,9 @@
+<?php  
+  require_once('includes/config.inc.php');
+  
+  $title = "Preguntas Frecuentes";
+
+  require_once(VIEW_PATH.'header.view.php');
+  require_once(VIEW_PATH.'faq.view.php');
+  require_once(VIEW_PATH.'footer.view.php');
+?>
