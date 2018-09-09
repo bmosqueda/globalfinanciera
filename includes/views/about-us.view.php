ed2e152f8eb4888c8bbd8d8bd21694351cd68871
brandon16mg <brandon16mg@gmail.com>
2018-09-09 17:38:48 -0500

    nada :v

---
+++ includes/views/about-us.view.php
@@ -0,0 +1,128 @@
+<link rel="stylesheet" href="/public/css/privacy.css">
+<section class="hero has-background-blue">
+  <div class="hero-body"></div>
+  <div class="hero-foot">
+    <div class="container">
+      <p class="title-hero is-size-1-desktop is-size-3-tablet is-size-4-mobile has-text-white">Nosotros</p>
+    </div>
+  </div>
+</section>
+<main class="section">
+  <div class="container">
+    <div class="columns is-centered">
+      <div class="column is-8-desktop is-10-tablet is-12-mobile">
+        <div class="content">
+          <p class="has-text-justified">Sociedad Patrimonial somos una SOFOM que otorga créditos personales inmediatos, no es determinante buró de crédito.</p>
+          <p class="has-text-justified">Contamos con más de 19 años de experiencia otorgando créditos personales y empresariales, brindando siempre la mejor atención a nuestros clientes.</p>
+          <p class="has-text-justified">Somos una empresa 100% regulada por dos instituciones, le invitamos a conocernos y saber más sobre todos los beneficios que ofrecemos para usted.</p>
+          <div>
+            <img src="/public/images/Logos/logo-10.jpeg" alt="Patrimonio social">
+          </div>
+          <br>
+          <h1>Misión</h1>
+          <p class="has-text-justified">Ser una empresa confiable cuya meta es brindar asesoría y dinero de forma rápida y eficaz a través del servicio especializado de asesores calificados e integración tecnológica.</p>
+          <h1>Visión</h1>
+          <p class="has-text-justified">Brindar opciones de crédito viables para cada uno de nuestros clientes y permitirles a través e ello lograr sus objetivos y/o proyectos económicos.</p>
+          <h1>Valores</h1>
+
+          <section class="container">  
+            <div class="columns features row is-multiline is-centered">
+              <div class="column is-4-desktop is-4-tablet is-12-mobile col">
+                  <div class="card is-shady text-mobile">
+                      <div class="card-image has-text-centered">
+                          <i class="fas fa-money-bill fa-5x"></i>
+                      </div>
+                      <div class="card-content">
+                          <div class="content">
+                              <h4>Créditos personales y empresariales</h4>
+                              <p>Préstamos en efectivo, no es determinante buró de crédito</p>
+                              <!-- <p><a href="#">Learn more</a></p> -->
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="column is-4-desktop is-4-tablet is-12-mobile col">
+                  <div class="card is-shady text-mobile">
+                      <div class="card-image has-text-centered">
+                          <i class="fas fa-user-shield fa-5x"></i>
+                      </div>
+                      <div class="card-content">
+                          <div class="content">
+                              <h4>Seguros</h4>
+                              <p>Te brindamos soluciones a la medida con la mayor flexibilidad</p>
+                              <!-- <p><a href="#">Learn more</a></p> -->
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="column is-4-desktop is-4-tablet is-12-mobile col">
+                  <div class="card is-shady text-mobile">
+                      <div class="card-image has-text-centered">
+                          <i class="fas fa-home fa-5x"></i>
+                      </div>
+                      <div class="card-content">
+                          <div class="content">
+                              <h4>Liberación de ahorro en subcuenta de vivienda</h4>
+                              <p>Te ayudamos a liberar el ahorro en tu subcuenta de vivienda</p>
+                              <!-- <p><a href="#">Learn more</a></p> -->
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
+              <div class="column is-4-desktop is-4-tablet is-12-mobile col">
+                  <div class="card is-shady text-mobile">
+                      <div class="card-image has-text-centered">
+                          <i class="fas fa-home fa-5x"></i>
+                      </div>
+                      <div class="card-content">
+                          <div class="content">
+                              <h4>Liberación de ahorro en subcuenta de vivienda</h4>
+                              <p>Te ayudamos a liberar el ahorro en tu subcuenta de vivienda</p>
+                              <!-- <p><a href="#">Learn more</a></p> -->
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="column is-4-desktop is-4-tablet is-12-mobile col">
+                  <div class="card is-shady text-mobile">
+                      <div class="card-image has-text-centered">
+                          <i class="fas fa-home fa-5x"></i>
+                      </div>
+                      <div class="card-content">
+                          <div class="content">
+                              <h4>Liberación de ahorro en subcuenta de vivienda</h4>
+                              <p>Te ayudamos a liberar el ahorro en tu subcuenta de vivienda</p>
+                              <!-- <p><a href="#">Learn more</a></p> -->
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="column is-4-desktop is-4-tablet is-12-mobile col">
+                  <div class="card is-shady text-mobile">
+                      <div class="card-image has-text-centered">
+                          <i class="fas fa-home fa-5x"></i>
+                      </div>
+                      <div class="card-content">
+                          <div class="content">
+                              <h4>Liberación de ahorro en subcuenta de vivienda</h4>
+                              <p>Te ayudamos a liberar el ahorro en tu subcuenta de vivienda</p>
+                              <!-- <p><a href="#">Learn more</a></p> -->
+                          </div>
+                      </div>
+                  </div>
+              </div>
+            </div>
+          </section>
+
+          <p class="has-text-justified">SERVICIO: Brindamos satisfacción a nuestros clientes.. </p>
+          <p class="has-text-justified">SINCERIDAD: Ofrecer transparencia en el trato hacia nuestros clientes. </p>
+          <p class="has-text-justified">RESPETO: Tratamos de forma digna a clientes y empleados. </p>
+          <p class="has-text-justified">HONESTIDAD: Actuamos de forma recta y congruente a nuestros principios. </p>
+          <p class="has-text-justified">RESPONSABILIDAD: Respondemos por nuestros actos. </p>
+          <p class="has-text-justified">TRABAJO EN EQUIPO: Realizamos labores conjuntas para llegar a nuestro objetivo que es el suyo también.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
